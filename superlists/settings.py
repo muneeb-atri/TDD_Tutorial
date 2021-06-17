f97d312aa834fddd0294b8869b7008d736668290
Muneeb Shahid <muneebsh@atrihub.io>
2021-06-17 10:24:43 -0700

    chore(Procfile): Travis CI and Heroku
    resolving static files issue.
    
    [ICTRAINING-707]

--- superlists/settings.py
+++ superlists/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if 'HEROKU_APP' in os.environ:
     # Activate Django-Heroku
